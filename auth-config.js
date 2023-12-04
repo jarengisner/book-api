03ba983a033aaf9427113b0eb9c402dce6f9098b
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-12-04 13:51:19 -0800

    Finishing touches on authentication

--- auth-config.js
+++ auth-config.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken'),
 
 const secret = process.env.JWTSECRET;
 
+require('./passport-config.js');
+
 const generateToken = (user) => {
   return jwt.sign(user, secret, {
     subject: user.username,
@@ -12,3 +14,29 @@ const generateToken = (user) => {
 };
 
 //check user against passport security
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        console.log(
+          'Something did not go right, or there is no user to be found'
+        );
+        res
+          .status(400)
+          .json({
+            message:
+              'Something went wrong / there is no user under these credentials',
+          });
+      }
+
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+
+        let token = generateToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    });
+  });
+};
